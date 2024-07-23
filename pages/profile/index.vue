d5f9e683014d82eb477d012fec14bec15ad66fab
Sajjad Mirshabi <m.sajjad1378@gmail.com>
2024-07-23 23:33:22 +0330

    Profile Layout

---
+++ pages/profile/index.vue
@@ -0,0 +1,13 @@
+<template>
+<div>
+  <strong>داشبورد</strong>
+  <hr class="my-4 opacity-20">
+
+</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout:'profile'
+})
+</script>
